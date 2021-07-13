610119a37a68e12b2774dc30713c2f8de2283831
mayank0508 <thakurmayank0508@gmail.com>
2021-07-14 02:32:00 +0530

    Save Btn made 🎯

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ const buttonClicked = document.getElementById('button-el');
 const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
 const deleteBtnEl = document.getElementById('delete-el');
+urlFromLocalStorage = localStorage.getItem('myUrl');
+urlConverted = JSON.parse(urlFromLocalStorage);
+let tabBtnEl = document.getElementById('save-el');
 // here we learn how to convert arrays into strings, using the JSON format
 
 // localStorage.setItem('myUrl', 'www.google.com');  // this a way to save a link in the local storage
 // console.log(localStorage.getItem('myUrl'));
 // localStorage.clear();
+
+const tabs = [{ url: 'http://www.linkedin.com/in/mayank0508' }];
+
+tabBtnEl.addEventListener('click', function () {    /// this is the way we make a save btn feature
+  
+  myUrl.push(tabs[0].url);
+  localStorage.setItem('myUrl', JSON.stringify(myUrl));
+  render(myUrl);
+});
 
 if (urlConverted) {
   myUrl = urlConverted;
@@ -39,11 +51,7 @@ deleteBtnEl.addEventListener('dblclick', function () {
   render(myUrl);
 });
 
-// urlFromLocalStorage = localStorage.getItem('myUrl');
-// urlConverted = JSON.parse(urlFromLocalStorage);
-// console.log(urlConverted);
-
-buttonClicked.addEventListener('click', function () {
+buttonClicked.addEventListener('click', function () {   /// this the way we push the data put inside the input element to the localStorage 💾
   myUrl.push(inputEl.value);
   inputEl.value = '';
   localStorage.setItem('myUrl', JSON.stringify(myUrl));
